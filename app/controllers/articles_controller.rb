fb89877bcb17605a2b2e2e996aeffe8bff3df58c
maxmay94 <maxmay94@gmail.com>
2022-08-29 18:46:02 -0400

    create new article complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
-  def def new
+  
+  def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
